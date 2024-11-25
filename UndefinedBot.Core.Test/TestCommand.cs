2c53457fd5c738ab303e45359f065b766cfb8d36
Chrise2024 <biter145@outlook.com>
2024-11-25 14:21:56 +0800

    add test project

---
+++ UndefinedBot.Core.Test/TestCommand.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using UndefinedBot.Core;
+using UndefinedBot.Core.Command;
+using UndefinedBot.Core.Command.Arguments.ArgumentType;
+
+namespace UndefinedBot.Core.Test
+{
+    public class TestCommand
+    {
+        private readonly UndefinedAPI _undefinedApi;
+        private readonly string _pluginName;
+
+        public TestCommand(string pluginName)
+        {
+            _undefinedApi = new(pluginName);
+            _pluginName = pluginName;
+            _undefinedApi.RegisterCommand("test")
+                .Description("指令帮助文档")
+                .ShortDescription("帮助")
+                .Usage("{0}help [指令名]")
+                .Example("{0}help help")
+                .Execute(async (ctx) =>
+                {
+                    Console.WriteLine(JsonConvert.SerializeObject(ctx.ArgumentReference));
+                }).Then(new VariableNode("sub1", new PosIntArgument())
+                    .Execute(async (ctx) =>
+                    {
+                        Console.WriteLine(JsonConvert.SerializeObject(ctx.ArgumentReference));
+                    }));
+            _undefinedApi.SubmitCommand();
+        }
+    }
+}
+
+
---
+++ UndefinedBot.Core/Readme.md
@@ -0,0 +1,25 @@
+﻿# UndefinedBot.Core
+
+The SDK of UndefinedBot.
+
+## Plugin Meta
+
+In `plugin.json`
+
+```json
+{
+  "name": "Help",
+  "description": "帮助",
+  "entry_file": "Command.Help.dll",
+  "entry_point": "HelpCommand"
+}
+```
+
+- `name` The name of plugin
+- `description` The description of plugin
+- `entry_file` The binary assembly of plugin
+- `entry_point` The plugin class to create instance
+
+## Command Develop
+
+
