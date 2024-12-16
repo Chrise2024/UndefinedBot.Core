e4a4c99b0af88c0271e92cf9d5e929db5f93cbf1
Chrise2024 <biter145@outlook.com>
2024-12-16 14:51:49 +0800

    change config format

---
+++ UndefinedBot.Net/NetWork/NetworkServiceCollection.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace UndefinedBot.Net.NetWork;
+
+public class NetworkServiceCollection(IServiceProvider services,
+    IConfiguration config,
+    ILogger<NetworkServiceCollection> logger
+    ) : IHostedService
+{
+    private readonly List<(IServiceScope, HttpServer)> _networkServices = [];
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        var scope = services.CreateScope();
+        var serviceProvider = scope.ServiceProvider;
+        HttpServer hs = new HttpServer(services.GetRequiredService<ILogger<HttpServer>>());
+        try
+        {
+            await hs.StartAsync(cancellationToken);
+            _networkServices.Add((scope,hs));
+        }
+        catch (Exception ex)
+        {
+            logger.LogError("Network Service Start Failed");
+            scope.Dispose();
+        }
+    }
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        foreach (var (scope, service) in _networkServices)
+        {
+            try
+            {
+                await service.StopAsync(cancellationToken);
+            }
+            catch (Exception e)
+            {
+                logger.LogError("Network Service Stop Failed");
+            }
+            finally
+            {
+                scope.Dispose();
+            }
+        }
+    }
+}
