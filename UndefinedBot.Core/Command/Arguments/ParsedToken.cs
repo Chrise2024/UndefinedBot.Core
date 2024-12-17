2847ac3667c55a9d80c8a949a6595a00528c78b6
Chrise2024 <biter145@outlook.com>
2024-12-17 14:17:53 +0800

    add factory method to create instance

--- UndefinedBot.Core/Command/Arguments/ParsedToken.cs
+++ UndefinedBot.Core/Command/Arguments/ParsedToken.cs
@@ -1,10 +1,41 @@
-﻿
+﻿using System.Text.RegularExpressions;
+
 namespace UndefinedBot.Core.Command.Arguments;
 
-[Serializable] public class ParsedToken
+[Serializable] public partial class ParsedToken
 {
-    public RawTokenTypes TokenType { get; set; } = RawTokenTypes.NormalContent;
-    public string Content { get; set; } = "";
+    public RawTokenTypes TokenType { get; private set; } = RawTokenTypes.NormalContent;
+    public string Content { get; private set; }
+
+    private ParsedToken(RawTokenTypes tokenType, string content)
+    {
+        if (tokenType == RawTokenTypes.NormalContent)
+        {
+            Content = content;
+            return;
+        }
+        Content = content;
+        TokenType = RawTokenTypes.CqCodeContent;
+    }
+
+    public static ParsedToken CreateCqToken(string content)
+    {
+        // if (!GetCqEntityRegex().IsMatch(content))
+        // {
+        //     throw new ArgumentException($"Invalid CQ Content: {content}");
+        // }
+        return new ParsedToken(RawTokenTypes.CqCodeContent,content);
+    }
+    public static ParsedToken CreateNormalToken(string content)
+    {
+        return new ParsedToken(RawTokenTypes.NormalContent,content);
+    }
+    public static ParsedToken CreateToken(RawTokenTypes tokenType,string content)
+    {
+        return tokenType == RawTokenTypes.NormalContent ? CreateNormalToken(content) : CreateCqToken(content);
+    }
+    [GeneratedRegex(@"\[CQ:\S+\]")]
+    private static partial Regex GetCqEntityRegex();
 }
 
 public enum RawTokenTypes
