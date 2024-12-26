c1c2f3e551e2e8f49baffa7fa1ea18ccc0684c96
Chrise2024 <biter145@outlook.com>
2024-12-26 15:51:43 +0800

    adapt to new version

--- UndefinedBot.Core.Test/TestCommand.cs
+++ UndefinedBot.Core.Test/TestCommand.cs
@@ -1,41 +1,38 @@
-﻿using UndefinedBot.Core.Command.CommandNodes;
+﻿using UndefinedBot.Core.Adapter;
+using UndefinedBot.Core.Command.CommandNodes;
 using UndefinedBot.Core.Command.Arguments.ArgumentType;
-using UndefinedBot.Core.Registry;
+using UndefinedBot.Core.Plugin;
 
 namespace UndefinedBot.Core.Test;
 
-public class TestCommand : IPluginInitializer
+public class TestCommand(PluginConfigData pluginConfig) : BasePlugin(pluginConfig)
 {
-    public void Initialize(UndefinedApi undefinedApi)
+    public override string Id => "Test";
+    public override string Name => "Test Plugin";
+    public override string TargetAdapterId => "OneBot11Adapter";
+    public override void Initialize()
     {
-        undefinedApi.RegisterCommand("test")
+        RegisterCommand("test")
             .Description("指令帮助文档")
             .ShortDescription("帮助")
             .Usage("{0}help [指令名]")
             .Example("{0}help help")
-            .Execute(async (ctx) => { Console.WriteLine("root"); })
+            .Execute(async (ctx,source) => { Console.WriteLine("root"); })
             .Then(new VariableNode("var1", new ReplyArgument())
                 .Then(new VariableNode("var2", new StringArgument())
-                    .Execute(async (ctx) =>
+                    .Execute(async (ctx,source) =>
                     {
                         //long v1 = IntegerArgument.GetInteger("var1", ctx);
                         //double v2 = NumberArgument.GetNumber("var2", ctx);
                         //Console.WriteLine(v1 * v2);
-                        QReply r = ReplyArgument.GetQReply("var1", ctx);
-                        string ptn = StringArgument.GetString("var2", ctx);
-                        Console.WriteLine(ptn);
                     })))
             .Then(new VariableNode("var3", new StringArgument())
                 .Then(new VariableNode("var4", new ImageArgument())
-                    .Execute(async (ctx) =>
+                    .Execute(async (ctx,source) =>
                     {
                         //long v3 = IntegerArgument.GetInteger("var3", ctx);
                         //long v4 = IntegerArgument.GetInteger("var4", ctx);
                         //Console.WriteLine(v3 + v4);
-                        string ptn = StringArgument.GetString("var3", ctx);
-                        QImage r = ImageArgument.GetImage("var4", ctx);
-                        Console.WriteLine(ptn);
                     })));
-        //UndefinedApi.SubmitCommand();
     }
 }
