d1b518f44e984b83fd266d386025aa2427fe6250
Chrise2024 <biter145@outlook.com>
2024-12-26 15:51:07 +0800

    optimize adapter and plugin's base class

--- UndefinedBot.Core/Adapter/BaseAdapter.cs
+++ UndefinedBot.Core/Adapter/BaseAdapter.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Nodes;
+﻿using System.Reflection;
+using System.Text.Json.Nodes;
 using UndefinedBot.Core.Command;
 using UndefinedBot.Core.Command.Arguments;
 using UndefinedBot.Core.Command.CommandSource;
@@ -15,26 +16,42 @@ public abstract class BaseAdapter(AdapterConfigData adapterConfig)
     protected AdapterLogger Logger => new(Name);
     protected AdapterConfigData AdapterConfig => adapterConfig;
     /// <summary>
-    /// For adapter invoke command
+    /// After processing message, use it to submit this event
     /// </summary>
     /// <param name="invokeProperties">Command's basic information</param>
     /// <param name="source">Command Source</param>
     /// <param name="tokens">Tokens, the body of the command</param>
     protected void SubmitCommandEvent(
-        PrimeInvokeProperties invokeProperties,
+        CommandInvokeProperties invokeProperties,
         BaseCommandSource source,
         List<ParsedToken> tokens
         )
     {
-        CommandEventBus.InvokeCommandEvent(invokeProperties.ImplementInvokeProperties(Id,Platform, Protocol, tokens), source);
+        CommandEventBus.InvokeCommandEvent(invokeProperties.Implement(Id,Platform, Protocol, tokens), source);
     }
     /// <summary>
-    /// Handle action invoked by command
+    /// Handle custom action invoked by command
     /// </summary>
-    public abstract void HandleCustomAction(string action, object paras);
-    public abstract void HandleDefaultAction(DefaultActionType action, object paras);
+    public abstract byte[]? HandleCustomAction(string action, byte[]? paras);
+    /// <summary>
+    /// Handle default action invoked by command
+    /// </summary>
+    public abstract byte[]? HandleDefaultAction(DefaultActionType action, byte[]? paras);
 }
-
+public enum DefaultActionType
+{
+    SendPrivateMsg = 0,
+    SendGroupMsg = 1,
+    RecallMessage = 2,
+    GetMessage = 3,
+    GetGroupMemberInfo = 4,
+    GetGroupMemberList = 5,
+    GroupMute = 6,
+    GroupKick = 7,
+}
+/// <summary>
+/// This Class records what write in config file
+/// </summary>
 [Serializable] public class AdapterConfigData : IEquatable<AdapterConfigData>
 {
     public string EntryFile { get; set; } = "";
@@ -48,5 +65,32 @@ public abstract class BaseAdapter(AdapterConfigData adapterConfig)
         return EntryFile == other?.EntryFile &&
                Description == other.Description &&
                CommandPrefix == other.CommandPrefix;
+    }
+}
+/// <summary>
+/// This class is for program record adapter's properties,include information defined in assembly
+/// </summary>
+[Serializable] public class AdapterProperties(BaseAdapter baseInstance,AdapterConfigData originData)
+{
+    public string Id => baseInstance.Id;
+    public string Name => baseInstance.Name;
+    public string Platform => baseInstance.Platform;
+    public string Protocol => baseInstance.Protocol;
+    public List<long> GroupIds => originData.GroupIds;
+    public string Description => originData.Description;
+    public string CommandPrefix => originData.CommandPrefix;
+}
+internal class AdapterInstance(object instance,MethodInfo dah,MethodInfo cah)
+{
+    private object Instance => instance;
+    private MethodInfo DefaultActionHandler => dah;
+    private MethodInfo CustomActionHandler => cah;
+    public byte[]? InvokeAction(DefaultActionType action, byte[]? paras = null)
+    {
+        return DefaultActionHandler.Invoke(Instance, [action, paras]) as byte[];
+    }
+    public byte[]? InvokeAction(string action, byte[]? paras = null)
+    {
+        return CustomActionHandler.Invoke(Instance, [action, paras]) as byte[];
     }
 }
---
+++ UndefinedBot.Core/Plugin/BasePlugin.cs
@@ -0,0 +1,60 @@
+﻿using System.Text.Json.Nodes;
+using UndefinedBot.Core.Command;
+using UndefinedBot.Core.Utils;
+
+namespace UndefinedBot.Core.Plugin;
+
+public abstract class BasePlugin(PluginConfigData pluginConfig)
+{
+    public abstract string Id { get; }
+    public abstract string Name { get; }
+    public abstract string TargetAdapterId { get; }
+    public abstract void Initialize();
+    protected PluginLogger Logger => new(Name);
+    protected UndefinedApi BaseApi => new(Name);
+    protected PluginConfigData PluginConfig => pluginConfig;
+    public List<CommandInstance> CommandInstances { get; } = [];
+    /// <summary>
+    /// Register Command
+    /// </summary>
+    /// <param name="commandName">
+    /// Command Name to be Called
+    /// </param>
+    /// <returns>
+    /// CommandInstance
+    /// </returns>
+    protected CommandInstance RegisterCommand(string commandName)
+    {
+        CommandInstance ci = new(commandName,TargetAdapterId);
+        CommandInstances.Add(ci);
+        return ci;
+    }
+}
+
+/// <summary>
+/// This Class records what write in config file
+/// </summary>
+[Serializable] public class PluginConfigData : IEquatable<PluginConfigData>
+{
+    public string EntryFile { get; set; } = "";
+    public string Description { get; set; } = "";
+    public List<long> GroupIds { get; set; } = [];
+    public JsonNode OriginalConfig { get; set; } = JsonNode.Parse("{}")!;
+
+    public bool Equals(PluginConfigData? other)
+    {
+        return EntryFile == other?.EntryFile &&
+               Description == other.Description;
+    }
+}
+/// <summary>
+/// This class is for program record plugin's properties,include information defined in assembly
+/// </summary>
+[Serializable] public class PluginProperties(BasePlugin baseInstance,PluginConfigData originData)
+{
+    public string Id => baseInstance.Id;
+    public string Name => baseInstance.Name;
+    public string TargetAdapterId => baseInstance.TargetAdapterId;
+    public List<long> GroupIds => originData.GroupIds;
+    public string Description => originData.Description;
+}
