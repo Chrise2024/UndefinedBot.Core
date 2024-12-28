42886fe00682be88d31896c93b2f784d18dd89e5
Chrise2024 <biter145@outlook.com>
2024-12-28 22:09:02 +0800

    make ParsedToken readonly,add comment text

--- UndefinedBot.Core/Command/Arguments/ParsedToken.cs
+++ UndefinedBot.Core/Command/Arguments/ParsedToken.cs
@@ -2,17 +2,19 @@
 namespace UndefinedBot.Core.Command.Arguments;
 
 /// <summary>
-/// Prepare to use 'ProtoBuf' to transmit communication content
+/// Prepare to use 'ProtoBuf' or string byte array to transmit communication content
 /// </summary>
 [Serializable] public class ParsedToken(ParsedTokenTypes tokenType, byte[] content)
 {
-    public ParsedTokenTypes TokenType { get; private set; } = tokenType;
-    public byte[] SerializedContent { get; private set; } = content;
+    public ParsedTokenTypes TokenType => tokenType;
+    public byte[] SerializedContent => content;
 }
 
 public enum ParsedTokenTypes
 {
-    Normal = 0,//Text formatted content(Serialize to byte array in UTF-8 encoding)
+    //Text formatted content(Serialize to byte array in UTF-8 encoding),including string,date and number
+    //number and date can be seen as string
+    Normal = 0,
     User = 1,
     Reply = 2,
     Image = 3,
